ea61bbef6d4dddf06895c9b2498e338b09ac0bf7
Hieu Chu <hieu.chu@tda-corp.co.jp>
2022-05-28 17:50:13 +0900

    Create custom HTML elements for carousel container & carousel items

---
+++ carouselContainer.js
@@ -0,0 +1,195 @@
+const carouselItemWidth = 150;
+const carouselFocusedItemWidth = 200;
+const carouselSmallItemWidth = 115;
+const carouselItemGap = 30;
+const carouselWidth =
+  4 * carouselItemGap +
+  2 * carouselItemWidth +
+  2 * carouselSmallItemWidth +
+  carouselFocusedItemWidth;
+const slideDistance = carouselItemWidth + carouselItemGap;
+const slideTime = 100; // ms
+let isSlideRight = true;
+let prevClickTime = Date.now();
+
+const carousel = document.getElementById("carousel");
+const prevButton = document.getElementById("prev-button");
+const nextButton = document.getElementById("next-button");
+
+const focusedItemIndexProxy = new Proxy(
+  { value: 3 },
+  {
+    set(target, prop, newValue, _receiver) {
+      if (prop != "value") {
+        return false;
+      }
+      updateFocusedItem(target.value, false);
+      target.value = newValue;
+      updateFocusedItem(newValue);
+      return true;
+    },
+  }
+);
+
+// const updateFocusedItem = (itemIndex, makeFocused = true) => {
+//   const getSelectorString = (index) =>
+//     "#carousel > .carousel-item:nth-child(" + index + ")";
+//   const focusedCarouselItem = document.querySelector(
+//     getSelectorString(itemIndex)
+//   );
+//   const smallCarouselItemLeft = document.querySelector(
+//     getSelectorString(itemIndex - 2)
+//   );
+//   const smallCarouselItemRight = document.querySelector(
+//     getSelectorString(itemIndex + 2)
+//   );
+//   if (makeFocused) {
+//     focusedCarouselItem.classList.add("focused-item");
+//     if (isSlideRight) {
+//       smallCarouselItemRight.classList.remove("hide");
+//     } else {
+//       smallCarouselItemLeft.classList.remove("hide");
+//     }
+//     smallCarouselItemLeft.classList.add("small-item");
+//     smallCarouselItemRight.classList.add("small-item");
+//   } else {
+//     focusedCarouselItem.classList.remove("focused-item");
+//     if (isSlideRight) {
+//       smallCarouselItemLeft.classList.add("hide");
+//     } else {
+//       smallCarouselItemRight.classList.add("hide");
+//     }
+//     smallCarouselItemLeft.classList.remove("small-item");
+//     smallCarouselItemRight.classList.remove("small-item");
+//   }
+// };
+// updateFocusedItem(3);
+
+// Use this function instead of carousel.scrollLeft
+// because the scrollLeft value is not completely updated until the scroll action has finished.
+// At the moment, there is not a standard method to determine if the scroll has finished.
+// A common workaround is to use a timer (~500ms) to wait for the completion of the scroll action.
+// const getScrollLeftValue = () => {
+//   return (
+//     (focusedItemIndexProxy.value - 3) * (carouselItemWidth + carouselItemGap)
+//   );
+// };
+
+// nextButton.onclick = () => {
+//   if (Date.now() - prevClickTime < 200) {
+//     return;
+//   }
+//   prevClickTime = Date.now();
+//   isSlideRight = true;
+//   prevButton.style.display = "block";
+//   carousel.scrollBy(slideDistance, 0);
+//   const timerId = setTimeout(() => {
+//     focusedItemIndexProxy.value += 1;
+//     if (getScrollLeftValue() + carouselWidth >= carousel.scrollWidth) {
+//       nextButton.style.display = "none";
+//     }
+//     clearTimeout(timerId);
+//   }, slideTime);
+// };
+
+// prevButton.onclick = () => {
+//   if (Date.now() - prevClickTime < 200) {
+//     return;
+//   }
+//   prevClickTime = Date.now();
+//   isSlideRight = false;
+//   nextButton.style.display = "block";
+//   carousel.scrollBy(-slideDistance, 0);
+//   const timerId = setTimeout(() => {
+//     focusedItemIndexProxy.value -= 1;
+//     if (getScrollLeftValue() == 0) {
+//       prevButton.style.display = "none";
+//     }
+//     clearTimeout(timerId);
+//   }, slideTime);
+// };
+
+const containerStyle = `
+  <style>
+    #carousel-container {
+        position: relative;
+    }
+
+    #carousel {
+        display: flex;
+        align-items: center;
+        gap: 30px;
+        overflow-x: hidden;
+        width: 870px;
+        margin: 0 auto;
+        padding-top: 50px;
+        padding-bottom: 50px;
+        scroll-behavior: smooth;
+        scrollbar-width: none;
+    }
+
+    #carousel::-webkit-scrollbar {
+        height: 0;
+    }
+
+    .hide {
+        visibility: hidden;
+    }
+
+    .arrow {
+        width: 15px;
+        height: 15px;
+        border-top: 5px solid;
+        border-right: 5px solid;
+        border-color: cadetblue;
+        border-top-right-radius: 5px;
+    }
+
+    .left-arrow {
+        transform: translateY(-50%) rotate(-135deg);
+    }
+
+    .right-arrow {
+        transform: translateY(-50%) rotate(45deg);
+    }
+
+    #prev-button,
+    #next-button {
+        position: absolute;
+        top: 50%;
+        cursor: pointer;
+    }
+
+    #prev-button {
+        left: -30px;
+    }
+
+    #next-button {
+        right: -30px;
+    }
+  </style>
+`;
+
+const containerTemplate = document.createElement("template");
+containerTemplate.innerHTML =
+  containerStyle +
+  `
+  <div id="carousel-container">
+    <div id="prev-button" class="arrow left-arrow"></div>
+    <div id="next-button" class="arrow right-arrow"></div>
+    <div id="carousel">
+      <slot></slot>
+    </div>
+  </div>`;
+
+class CarouselContainer extends HTMLElement {
+  // connectedCallback() {}
+  constructor() {
+    super();
+    console.log(this.children.length);
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(containerTemplate.content.cloneNode(true));
+  }
+}
+
+window.customElements.define("carousel-container", CarouselContainer);
---
+++ carouselItem.js
@@ -0,0 +1,89 @@
+const itemStyle = `
+<style>
+     .carousel-item {
+        width: 150px;
+        height: 150px;
+        min-width: 150px;
+        perspective: 1000px;
+        opacity: 0.85;
+    }
+
+    .focused-item {
+        height: 200px;
+        min-width: 200px;
+        opacity: 1;
+    }
+
+    .small-item {
+        height: 115px;
+        min-width: 115px;
+        opacity: 0.6;
+    }
+
+    .carousel-item>.cards-container {
+        width: 100%;
+        height: 100%;
+        position: relative;
+        transition: transform 1s;
+        transform-style: preserve-3d;
+    }
+
+    .carousel-item>.cards-container:hover {
+        transform: translateY(-20px) rotateY(180deg);
+    }
+
+    .carousel-item>.cards-container>.front-card,
+    .carousel-item>.cards-container>.back-card {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 5px;
+        font-size: 32px;
+        backface-visibility: hidden;
+    }
+
+    .carousel-item>.cards-container>.front-card {
+        background-color: cadetblue;
+        color: whitesmoke;
+    }
+
+    .carousel-item>.cards-container>.back-card {
+        transform: rotateY(180deg);
+        background-color: #ffc107;
+        color: whitesmoke;
+    }
+
+    .small-item>.cards-container>div.back-card {
+        font-size: 24px;
+    }
+</style>
+`;
+const itemTemplate = document.createElement("template");
+itemTemplate.innerHTML =
+  itemStyle +
+  `
+  <div class="carousel-item">
+    <div class="cards-container">
+      <div class="front-card">1</div>
+      <div class="back-card">Back card</div>
+    </div>
+  </div>`;
+
+class CarouselItem extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(itemTemplate.content.cloneNode(true));
+  }
+  connectedCallback() {
+    const frontCardContent = this.attributes.front.value;
+    const backCardContent = this.attributes.back.value;
+    this.shadowRoot.querySelector(".front-card").innerHTML = frontCardContent;
+    this.shadowRoot.querySelector(".back-card").innerHTML = backCardContent;
+  }
+}
+
+window.customElements.define("carousel-item", CarouselItem);
